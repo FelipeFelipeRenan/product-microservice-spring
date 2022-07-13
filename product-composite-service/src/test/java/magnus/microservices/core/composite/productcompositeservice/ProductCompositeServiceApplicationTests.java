230bda15646d65fda01245c5aaf14503bad04191
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2022-07-13 19:57:44 -0300

    DEV - Adding compposite and recommendation services

---
+++ product-composite-service/src/test/java/magnus/microservices/core/composite/productcompositeservice/ProductCompositeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package magnus.microservices.core.composite.productcompositeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCompositeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
