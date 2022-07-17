065f16223432148b5ab6ca22037c00dfdea96006
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2022-07-17 18:53:06 -0300

    DEV - Adding error handling for product service

--- microservice/product-service/src/main/java/felipe/microservices/core/product/services/ProductServiceImpl.java
+++ microservice/product-service/src/main/java/felipe/microservices/core/product/services/ProductServiceImpl.java
@@ -4,6 +4,8 @@ package felipe.microservices.core.product.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import felipe.api.core.product.ProductService;
+import felipe.api.exceptions.InvalidInputException;
+import felipe.api.exceptions.NotFoundException;
 import felipe.util.http.ServiceUtil;
 import felipe.api.core.product.Product;
 
@@ -19,6 +21,16 @@ public class ProductServiceImpl implements ProductService{
 
     @Override
     public Product getProduct(int productId) {
+
+        if(productId < 1){
+            throw new InvalidInputException("Invalid productId: " + productId);
+
+        }
+
+        if(productId == 12){
+            throw new NotFoundException("No product found for productId: " + productId);
+        }
+
         return new Product(productId, "name-" + productId, 123, serviceUtil.getServiceAddress());
     }
     
