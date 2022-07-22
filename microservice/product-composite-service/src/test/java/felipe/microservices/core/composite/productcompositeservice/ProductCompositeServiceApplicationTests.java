54dac81d28c70509b8330007e41c002b3bbab9d5
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2022-07-22 19:41:54 -0300

    DEV - Adding getProductById test

--- microservice/product-composite-service/src/test/java/felipe/microservices/core/composite/productcompositeservice/ProductCompositeServiceApplicationTests.java
+++ microservice/product-composite-service/src/test/java/felipe/microservices/core/composite/productcompositeservice/ProductCompositeServiceApplicationTests.java
@@ -1,13 +1,38 @@
 package felipe.microservices.core.composite.productcompositeservice;
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest
 class ProductCompositeServiceApplicationTests {
+	@Autowired
+	private WebTestClient client;
 
+	private static final int PRODUCT_ID_OK = 1;
+	private static final int PRODUCT_ID_NOT_FOUND = 2;
+	private static final int PRODUCT_ID_INVALID = 3;
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void getProductByIs(){
+		client.get()
+			.uri("/product-composite/" + PRODUCT_ID_OK)
+			.accept(APPLICATION_JSON)
+			.exchange()
+			.expectStatus().isOk()
+			.expectHeader().contentType(APPLICATION_JSON)
+			.expectBody()
+			.jsonPath("$.productId").isEqualTo(PRODUCT_ID_OK)
+			.jsonPath("$.recommendation.lenght()").isEqualTo(1)
+			.jsonPath("$.review.lenght()").isEqualTo(1);
 	}
 
 }
