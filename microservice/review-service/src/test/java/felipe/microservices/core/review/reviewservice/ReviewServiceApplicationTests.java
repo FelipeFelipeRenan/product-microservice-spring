b422b0c80f632f47c4c09f6f55bf8848e2d49614
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2022-07-23 17:37:49 -0300

    DEV - Adding tests for recommendation microsservice

--- microservice/review-service/src/test/java/felipe/microservices/core/review/reviewservice/ReviewServiceApplicationTests.java
+++ microservice/review-service/src/test/java/felipe/microservices/core/review/reviewservice/ReviewServiceApplicationTests.java
@@ -1,5 +1,10 @@
 package felipe.microservices.core.review.reviewservice;
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
