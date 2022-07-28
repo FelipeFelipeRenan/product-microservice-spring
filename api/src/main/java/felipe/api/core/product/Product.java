d9b5a709761d7e9db0f0a8fc97601f0a6ed2457a
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2022-07-28 19:28:30 -0300

    DEV - Adding more methods to product service and mapper

--- api/src/main/java/felipe/api/core/product/Product.java
+++ api/src/main/java/felipe/api/core/product/Product.java
@@ -3,10 +3,10 @@ package felipe.api.core.product;
 
 public class Product {
 
-    private final int productId;
-    private final String name;
-    private final int weight;
-    private final String serviceAddress;
+    private  int productId;
+    private  String name;
+    private  int weight;
+    private  String serviceAddress;
 
     public Product() {
         productId = 0;
@@ -37,6 +37,24 @@ public class Product {
     public String getServiceAddress() {
         return serviceAddress;
     }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setServiceAddress(String serviceAddress) {
+        this.serviceAddress = serviceAddress;
+    }
+
+    
 
     
     
--- microservice/product-service/src/main/java/felipe/microservices/core/product/services/ProductServiceImpl.java
+++ microservice/product-service/src/main/java/felipe/microservices/core/product/services/ProductServiceImpl.java
@@ -3,10 +3,13 @@ package felipe.microservices.core.product.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.web.bind.annotation.RestController;
 import felipe.api.core.product.ProductService;
 import felipe.api.exceptions.InvalidInputException;
 import felipe.api.exceptions.NotFoundException;
+import felipe.microservices.core.product.persistence.ProductEntity;
+import felipe.microservices.core.product.persistence.ProductRepository;
 import felipe.util.http.ServiceUtil;
 import felipe.api.core.product.Product;
 
@@ -17,9 +20,15 @@ public class ProductServiceImpl implements ProductService {
 
     private final ServiceUtil serviceUtil;
 
+    private final ProductRepository repository;
+
+    private final ProductMapper mapper;
+
     @Autowired
-    public ProductServiceImpl(ServiceUtil serviceUtil) {
+    public ProductServiceImpl(ProductRepository repository, ProductMapper mapper,  ServiceUtil serviceUtil) {
+        this.repository = repository;
         this.serviceUtil = serviceUtil;
+        this.mapper = mapper;
     }
 
     @Override
@@ -29,11 +38,28 @@ public class ProductServiceImpl implements ProductService {
         if (productId < 1) {
             throw new InvalidInputException("Invalid productId: " + productId);
         }
+        ProductEntity entity = repository.findByProductId(productId)
+            .orElseThrow(() -> new NotFoundException("No product found for productId: " + productId));
 
-        if (productId == 13) {
-            throw new NotFoundException("No product found for productId: " + productId);
+        Product response = mapper.entityToApi(entity);
+        response.setServiceAddress(serviceUtil.getServiceAddress());
+        return response;
         }
 
-        return new Product(productId, "name-" + productId, 123, serviceUtil.getServiceAddress());
+    @Override
+    public Product createProduct(Product body){
+        try {
+            ProductEntity entity = mapper.apiToEntity(body);
+            ProductEntity newEntity = repository.save(entity);
+            return mapper.entityToApi(newEntity);
+        } catch (DuplicateKeyException dke) {
+            throw new InvalidInputException("Duplicated key, Product Id: " + body.getProductId());
+        
+        }
+    }
+
+    @Override
+    public void deleteProduct(int productId){
+        repository.findByProductId(productId).ifPresent(e -> repository.delete(e));
     }
 }
