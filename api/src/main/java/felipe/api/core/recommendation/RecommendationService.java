086ba782d932354fe929712bd745230dfd1bc13a
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2022-07-28 18:45:49 -0300

    DEV - Adding more methods to services interfaces

--- api/src/main/java/felipe/api/core/recommendation/RecommendationService.java
+++ api/src/main/java/felipe/api/core/recommendation/RecommendationService.java
@@ -2,21 +2,28 @@ package felipe.api.core.recommendation;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 public interface RecommendationService {
 
-    /**
-     * Sample usage: "curl $HOST:$PORT/recommendation?productId=1".
-     *
-     * @param productId Id of the product
-     * @return the recommendations of the product
-     */
-    @GetMapping(
-      value = "/recommendation",
-      produces = "application/json")
-    List<Recommendation> getRecommendations(
+  @GetMapping(value = "/recommendation", produces = "application/json")
+  List<Recommendation> getRecommendations(
       @RequestParam(value = "productId", required = true) int productId);
-  }
+
+
+  @PostMapping(
+    value = "/recommendation",
+    consumes = "application/json",
+    produces = "application/json"
+  )
+  Recommendation createRecommendations(@RequestBody Recommendation body);
+
+  @DeleteMapping(value = "/recommendation")
+  void deleteRecommendations(@RequestParam(value = "productId", required = true) int productId);
+
+}
--- api/src/main/java/felipe/api/core/review/ReviewService.java
+++ api/src/main/java/felipe/api/core/review/ReviewService.java
@@ -3,20 +3,32 @@ package felipe.api.core.review;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import felipe.api.core.recommendation.Recommendation;
 
 @Service
 public interface ReviewService {
 
-    /**
-     * Sample usage: "curl $HOST:$PORT/review?productId=1".
-     *
-     * @param productId Id of the product
-     * @return the reviews of the product
-     */
     @GetMapping(
       value = "/review",
       produces = "application/json")
     List<Review> getReviews(@RequestParam(value = "productId", required = true) int productId);
+
+    @PostMapping(
+      value = "/review",
+      consumes = "application/json",
+      produces = "application/json"
+    )
+    Review createRecommendation(@RequestBody Review body);
+
+
+    @DeleteMapping(value = "/review")
+    void deleteReviews(@RequestParam(value = "productId", required = true) int productId);
+
+
   }
