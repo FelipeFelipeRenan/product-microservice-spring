3ea175e032c0530c9b00ebd88d39732e6d815f77
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2022-07-30 15:13:16 -0300

    DEV - Adding post and delete methods to composite interface

--- api/src/main/java/felipe/api/composite/product/ProductCompositeService.java
+++ api/src/main/java/felipe/api/composite/product/ProductCompositeService.java
@@ -1,7 +1,10 @@
 package felipe.api.composite.product;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -27,4 +30,21 @@ public interface ProductCompositeService {
   })
   @GetMapping(value = "/product-composite/{productId}", produces = "application/json")
   ProductAggregate getProduct(@PathVariable int productId);
+
+  @Operation(summary = "${api.product-composite.create-composite-product.description}", description = "${api.product-composite.create-composite-product.notes}")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "400", description = "${api.responseCodes.badRequest.description}"),
+      @ApiResponse(responseCode = "422", description = "${api.responseCodes.unprocessableEntity.description}")
+  })
+  @PostMapping(value = "/product-composite", consumes = "application/json")
+  void createProduct(@RequestBody ProductAggregate body);
+
+  @Operation(summary = "${api.product-composite.delete-composite-product.description}", description = "${api.product-composite.delete-composite-product.notes}")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "400", description = "${api.responseCodes.badRequest.description}"),
+      @ApiResponse(responseCode = "422", description = "${api.responseCodes.unprocessableEntity.description}")
+  })
+  @DeleteMapping(value = "/product-composite/{productId}")
+  void deleteProduct(@PathVariable int productId);
+
 }
